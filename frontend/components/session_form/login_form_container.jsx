1a6429caef5e82d1e08f7857dba39a30a8e9fe44
Michelle Addai <88467155+mickeysaddai@users.noreply.github.com>
2021-12-26 19:38:34 -0500

    session form updated

--- frontend/components/session_form/login_form_container.jsx
+++ frontend/components/session_form/login_form_container.jsx
@@ -5,6 +5,11 @@ import SessionForm from './session_form';
 const mapStateToProps = (state) => ({
     errors: state.errors.session,
     formType: 'login',
+      demoUser: {
+        username: 'mickey',
+        password: 'password123'
+    },
+
 })
 const mapDispatchToProps = (dispatch) => ({
     processForm: (user) => dispatch(login(user))
